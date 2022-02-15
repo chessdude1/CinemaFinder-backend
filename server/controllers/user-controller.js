91eda0bbf65bc8974820111ddd38dee6bc5385b8
chessdude1 <mitia.platonow2011@yandex.ru>
2022-02-15 15:46:02 +0300

    fix: if there is no picture, there should be the possibility of registration

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -10,9 +10,12 @@ class UserController {
             if (!errors.isEmpty()) {
                 return next(ApiError.BadRequest('Ошибка при валидации', errors.array()))
             }
-            const picture = req.files.picture
             const { email, password, favoriteFilms } = req.body;
-            const fileName = fileService.saveFile(picture)
+            const fileName = '';
+            if (req.files) {
+                const picture = req.files.picture
+                fileName = fileService.saveFile(picture)
+            }
             const userData = await userService.registration(email, password, favoriteFilms, fileName);
 
             res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true })
